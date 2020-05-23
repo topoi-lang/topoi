47cba8c1e35aae7a35b4c20f0d77cb380ffa2c10
zypeh <zypeh.geek@gmail.com>
2020-05-23 18:20:50 +0800

    lexer done

--- rust/lexer/src/lib.rs
+++ rust/lexer/src/lib.rs
@@ -1,8 +1,11 @@
 extern crate logos;
 use logos::Logos;
+use std::ops::Range;
 
 #[derive(Logos, Debug, PartialEq)]
-enum Token {
+pub enum Token {
+    EndOfProgram,
+
     #[token(",")]
     Comma,
     #[token(".")]
@@ -23,8 +26,8 @@ enum Token {
     #[token("}")]
     BraceClose,
 
-    #[regex("[a-zA-Z_$][a-zA-Z0-9_'$]*")]
-    Identifier,
+    #[regex("[a-zA-Z_$][a-zA-Z0-9_'$]*", |lex| lex.slice().to_owned())]
+    Identifier(String),
 
     #[token("true")]
     LiteralTrue,
@@ -41,16 +44,60 @@ enum Token {
     #[regex("succ|pred|zero")]
     ReservedKeyword,
 
-    #[regex("if")]
+    #[token("if")]
     KwIf,
-    #[regex("then")]
+    #[token("then")]
     KwThen,
-    #[regex("else")]
+    #[token("else")]
     KwElse,
 
+    #[token("\n")]
+    Newline,
+
+    #[regex("--[^\r\n]*(\r\n|\n)?", |lex| lex.slice().to_owned())]
+    Comment(String),
+
     #[error]
-    #[regex(r"[ ]", logos::skip)] // skip whitespace
+    #[regex("[ ]", logos::skip)]
     UnexpectedToken,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct TopoiLexer<'source> {
+    source: &'source str,
+
+    /// Result tokens
+    tokens: Vec<(Token, Range<usize>)>,
+
+    errors: Vec<(Token, Range<usize>)>,
+}
+
+impl<'source> TopoiLexer<'source> {
+    pub fn new(source: &'source str) -> Self {
+        TopoiLexer {
+            source,
+            tokens: vec![],
+            errors: vec![],
+        }
+    }
+
+    pub fn lex(mut self) -> Self {
+        self.tokens = Token::lexer(self.source)
+            .spanned()
+            .map(|token_with_span| match &token_with_span {
+                (Token::UnexpectedToken, span) => {
+                    self.errors.push((Token::UnexpectedToken, span.clone()));
+                    token_with_span
+                }
+                _ => token_with_span,
+            })
+            .collect();
+        self
+    }
+
+    pub fn tokens(self) -> Vec<(Token, Range<usize>)> {
+        self.tokens
+    }
 }
 
 #[cfg(test)]
@@ -67,7 +114,7 @@ mod lexer {
 
     #[test]
     fn ident_lex() {
-        let input = "succ 1";
+        let input = "succ 1 apple";
         let mut lex = Token::lexer(input);
 
         assert_eq!(lex.next(), Some(Token::ReservedKeyword));
@@ -75,5 +122,38 @@ mod lexer {
 
         assert_eq!(lex.next(), Some(Token::LiteralInteger));
         assert_eq!(lex.slice(), "1");
+
+        assert_eq!(lex.next(), Some(Token::Identifier("apple".to_string())));
+        assert_eq!(lex.slice(), "apple");
+    }
+
+    #[test]
+    fn comment_lex() {
+        let input = "succ\n-- aaaaa";
+        let mut lex = Token::lexer(input);
+
+        assert_eq!(lex.next(), Some(Token::ReservedKeyword));
+        assert_eq!(lex.slice(), "succ");
+        assert_eq!(lex.next(), Some(Token::Newline));
+        assert_eq!(lex.next(), Some(Token::Comment("-- aaaaa".to_owned())));
+        assert_eq!(lex.next(), None);
+    }
+
+    #[test]
+    fn lex_topoi() {
+        let input = "succ 1 \\";
+        let topoi_lexer = TopoiLexer::new(input).lex();
+        assert_eq!(
+            topoi_lexer,
+            TopoiLexer {
+                source: input,
+                tokens: vec![
+                    (Token::ReservedKeyword, 0..4),
+                    (Token::LiteralInteger, 5..6),
+                    (Token::UnexpectedToken, 7..8),
+                ],
+                errors: vec![(Token::UnexpectedToken, 7..8)],
+            }
+        );
     }
 }
---
+++ rust/parser/src/lib.rs
@@ -0,0 +1,4 @@
+extern crate bumpalo;
+extern crate logos;
+extern crate topoi_ast as ast;
+extern crate topoi_lexer as lexer;
